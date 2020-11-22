6f77dd05c05e31fc18b455383ad9513ef9d7380d
Shay Reichert <shay.reichert@outlook.fr>
2020-11-22 15:58:23 +0100

    fix keyup on keydown and quick refacto

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,10 +1,11 @@
-// On key press, trigger sound
-document.addEventListener("keyup", (event) => {
-  const keyCode = event.keyCode;
+document.addEventListener("keydown", handleKeyPressed);
+
+function handleKeyPressed(event) {
+  const keyCode = event.keyCode || event.key;
   const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
   const key = document.querySelector(`div[data-key="${keyCode}"]`);
   playSound(sound, key);
-});
+}
 
 function playSound(sound, key) {
   if (sound !== null) {
@@ -16,6 +17,6 @@ function playSound(sound, key) {
 }
 
 function removeClass(event) {
-  if (event.propertyName !== "transform") return;
+  if (event.propertyName !== "transform") return; // skip if it's not a transform transition
   event.target.classList.remove("playing");
 }
