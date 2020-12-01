d4402e9508fc24c4a8d2a7855ba52dd154a00bc4
Shay Reichert <shay.reichert@outlook.fr>
2020-12-01 12:00:29 +0100

    refacto 10

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,21 +1,19 @@
-      const checkboxes = document.querySelectorAll(
-        '.inbox input[type="checkbox"]'
-      );
-      let lastChecked;
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
 
-      checkboxes.forEach((checkbox) =>
-        checkbox.addEventListener("click", handleCheck)
-      );
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
 
-      function handleCheck(event) {
-        let inBetween = false;
-        if (event.shiftKey && this.checked) {
-          checkboxes.forEach((checkbox) => {
-            if (checkbox === this || checkbox === lastChecked) {
-              inBetween = !inBetween;
-            }
-            inBetween && (checkbox.checked = true);
-          });
-        }
-        lastChecked = this;
+function handleCheck(event) {
+  let inBetween = false;
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
       }
+      inBetween && (checkbox.checked = true);
+    });
+  }
+  lastChecked = this;
+}
