702e6cfdeeb44c703ef6b42c326f043a58ce0a38
Shay Reichert <shay.reichert@outlook.fr>
2020-12-01 11:54:47 +0100

    [Day 10] - 10 done

---
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,21 @@
+      const checkboxes = document.querySelectorAll(
+        '.inbox input[type="checkbox"]'
+      );
+      let lastChecked;
+
+      checkboxes.forEach((checkbox) =>
+        checkbox.addEventListener("click", handleCheck)
+      );
+
+      function handleCheck(event) {
+        let inBetween = false;
+        if (event.shiftKey && this.checked) {
+          checkboxes.forEach((checkbox) => {
+            if (checkbox === this || checkbox === lastChecked) {
+              inBetween = !inBetween;
+            }
+            inBetween && (checkbox.checked = true);
+          });
+        }
+        lastChecked = this;
+      }
